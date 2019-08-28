094ecd490a46ad026271de17eb78aae0bce1e323
seongmock <yooka88@gmail.com>
2019-08-28 17:56:02 +0900

    Update Tkinter

--- CopyTrans.py
+++ CopyTrans.py
@@ -19,41 +19,62 @@ import tkinter;
 from googletrans import Translator;
 import threading
 
+window = tkinter.Tk();
+label_txt = tkinter.StringVar();
+label_txt.set("Google Translate");
+# tkinter.Label(window, text="Maximum 가로길이(글자수)").grid(row=0);
+# tkinter.Entry(window).grid(row=0, column=1)l
+LABEL = tkinter.Label(window, textvariable=label_txt, padx=20, pady=20);
+LABEL.pack();
+prv_text="";
 
+
+def CutLongString(text):
+    cnt = 0;
+    new_line = ""
+    for char in text:
+        if(char=='\n'):
+            cnt = 0;
+        elif(cnt==100):
+            cnt = 0;
+            new_line = new_line + '\n';
+        else:
+            cnt += 1;
+        new_line = new_line + char;
+
+    return new_line;
 
 def GoogleTrans(text):
     translator = Translator();
     trans_text = translator.translate(text, dest='ko').text;
     return trans_text;
     
+def GetClip():
+    # print("Function CAll");
+    global window, prv_text,label_txt;
+    text = clipboard.paste();
+    if(prv_text!=text) :
+        # print(text);
+        trans_text = GoogleTrans(text);
+        trans_text = CutLongString(trans_text);
+        label_txt.set(trans_text);
+        
+        window.attributes("-topmost", True)
+        window.attributes("-topmost", False)
+        window.update() 
+        window.deiconify()
+
+        prv_text = text;
+    window.after(500, GetClip);
+
+
 
 
 if __name__ == "__main__":
-        
-    prv_text="";
-    window = tkinter.Tk();
     window.minsize(500, 80);
     window.resizable(False, False);
     window.title("Google Translate - BBOMI KIM");
     
-    while(1):        
-        text = clipboard.paste();
-        if(prv_text!=text) :
-            # print(text);
-            trans_text = GoogleTrans(text);
-            try: 
-                LABEL.pack_forget();
-            except:
-                pass;
-
-
-            LABEL = tkinter.Label(window, text=trans_text, padx=20, pady=20);
-            LABEL.pack();
-            window.attributes("-topmost", True)
-            window.attributes("-topmost", False)
-            window.update()
-
-            prv_text = text;
-
-        time.sleep(0.5);
+    window.after(500, GetClip);
+    window.mainloop();
 
